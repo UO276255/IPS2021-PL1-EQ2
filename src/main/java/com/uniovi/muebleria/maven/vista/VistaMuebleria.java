635a2f006f5de1a2ba4c60efa4cae3858199be33
Adrian <uo279062@uniovi.es>
2021-10-11 19:19:01 +0200

    Asignacion con vista asigna transporte

--- src/main/java/com/uniovi/muebleria/maven/vista/VistaMuebleria.java
+++ src/main/java/com/uniovi/muebleria/maven/vista/VistaMuebleria.java
@@ -6,14 +6,11 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import com.uniovi.muebleria.maven.modelo.transportista.TransportistaDTO;
 
 public class VistaMuebleria extends JFrame {
 
