15bae220ed795a3a7dab3f4edb279fb6eab1500d
Adrian <uo279062@uniovi.es>
2021-10-12 17:18:28 +0200

    a

--- src/main/java/com/uniovi/muebleria/maven/vista/VistaMuebleria.java
+++ src/main/java/com/uniovi/muebleria/maven/vista/VistaMuebleria.java
@@ -5,16 +5,10 @@ import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import javax.swing.JButton;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class VistaMuebleria extends JFrame {
 
