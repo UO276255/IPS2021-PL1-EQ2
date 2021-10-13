a5c7009e989ecfbc2f8a699324afb24c9a9665f0
Adrian <uo279062@uniovi.es>
2021-10-13 23:00:54 +0200

    productoDTO

--- src/main/java/com/uniovi/muebleria/maven/vista/VistaAsignaTransporte.java
+++ src/main/java/com/uniovi/muebleria/maven/vista/VistaAsignaTransporte.java
@@ -1,13 +1,14 @@
 package com.uniovi.muebleria.maven.vista;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import com.uniovi.muebleria.maven.modelo.transportista.TransportistaDTO;
-
-import javax.swing.JComboBox;
-import javax.swing.JButton;
+import javax.swing.JList;
 import javax.swing.JPanel;
+
+import com.uniovi.muebleria.maven.modelo.producto.ProductoDTO;
+import com.uniovi.muebleria.maven.modelo.transportista.TransportistaDTO;
 
 public class VistaAsignaTransporte extends JFrame{
 	
@@ -21,6 +22,14 @@ public class VistaAsignaTransporte extends JFrame{
 	private JLabel lblListaTransportistas;
 	private JComboBox<TransportistaDTO> comboBoxListaTransportistas;
 	private JButton btnAceptaTransp;
+	private JPanel panelProductos;
+	private JPanel panelMontados;
+	private JLabel lblProductos;
+	private JLabel lblMontados;
+	private JList<ProductoDTO> listProductos;
+	private JList listMontados;
+	private JButton btnTransportados;
+	private JButton btnMontados;
 	
 	public VistaAsignaTransporte() {
 		setTitle("Muebleria");
@@ -37,6 +46,8 @@ public class VistaAsignaTransporte extends JFrame{
 			panelPrncpl.add(getLblListaTransportistas());
 			panelPrncpl.add(getComboBoxListaTransportistas());
 			panelPrncpl.add(getBtnAceptaTransp());
+			panelPrncpl.add(getPanelProductos());
+			panelPrncpl.add(getPanelMontados());
 		}
 		return panelPrncpl;
 	}
@@ -60,5 +71,69 @@ public class VistaAsignaTransporte extends JFrame{
 			btnAceptaTransp.setBounds(44, 445, 200, 23);
 		}
 		return btnAceptaTransp;
+	}
+	private JPanel getPanelProductos() {
+		if (panelProductos == null) {
+			panelProductos = new JPanel();
+			panelProductos.setBounds(44, 119, 317, 295);
+			panelProductos.setLayout(null);
+			panelProductos.add(getLblProductos());
+			panelProductos.add(getListProductos());
+			panelProductos.add(getBtnTransportados());
+		}
+		return panelProductos;
+	}
+	private JPanel getPanelMontados() {
+		if (panelMontados == null) {
+			panelMontados = new JPanel();
+			panelMontados.setBounds(371, 119, 317, 295);
+			panelMontados.setLayout(null);
+			panelMontados.add(getLblMontados());
+			panelMontados.add(getListMontados());
+			panelMontados.add(getBtnMontados());
+		}
+		return panelMontados;
+	}
+	private JLabel getLblProductos() {
+		if (lblProductos == null) {
+			lblProductos = new JLabel("Productos transportados");
+			lblProductos.setBounds(10, 0, 307, 20);
+		}
+		return lblProductos;
+	}
+	private JLabel getLblMontados() {
+		if (lblMontados == null) {
+			lblMontados = new JLabel("Productos montados");
+			lblMontados.setBounds(10, 0, 307, 21);
+		}
+		return lblMontados;
+	}
+	private JList getListProductos() {
+		if (listProductos == null) {
+			listProductos = new JList();
+			listProductos.setBounds(0, 21, 317, 248);
+		}
+		return listProductos;
+	}
+	private JList getListMontados() {
+		if (listMontados == null) {
+			listMontados = new JList();
+			listMontados.setBounds(0, 22, 317, 248);
+		}
+		return listMontados;
+	}
+	private JButton getBtnTransportados() {
+		if (btnTransportados == null) {
+			btnTransportados = new JButton("Añadir a productos para montar");
+			btnTransportados.setBounds(0, 272, 317, 23);
+		}
+		return btnTransportados;
+	}
+	private JButton getBtnMontados() {
+		if (btnMontados == null) {
+			btnMontados = new JButton("Quitar de productos para montar");
+			btnMontados.setBounds(0, 272, 317, 23);
+		}
+		return btnMontados;
 	}
 }
